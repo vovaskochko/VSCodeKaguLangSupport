3a0f2b03f2e9d343739cda8ff28bbe4f88f74c06
Volodymyr Skochko <vovaskochko@gmail.com>
2025-01-09 00:44:55 +0200

    change Kagu asm extension to .kga other fixes

---
+++ prepare_release.sh
@@ -0,0 +1,5 @@
+https://code.visualstudio.com/api/working-with-extensions/publishing-extension
+npm install -g @vscode/vsce
+# npm run compile # If extension contains TypeScript sources
+vsce package
+vsce publish
